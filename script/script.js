65a4cfabb6212ce48bae90a6d4d80a4ddee11788
Stan Bratuszewski <145658170+sbratuszewski@users.noreply.github.com>
2024-11-02 15:36:27 +0000

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -1,2 +1,11 @@
+//display current year in footer
 const currentYear = new Date().getUTCFullYear();
-document.getElementById("date").innerHTML = currentYear;
+$("#date").html(currentYear);
+
+// return to the top arrow
+// arrow.addEventListener("click", () => {
+//   window.scroll({
+//     top: 0,
+//     behavior: "smooth",
+//   });
+// });
