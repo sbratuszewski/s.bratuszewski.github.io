97f29ef3ba06c05d353ab37befdc109110b8d816
Stan Bratuszewski <145658170+sbratuszewski@users.noreply.github.com>
2024-11-02 20:27:38 +0000

    update in navigation

--- script/script.js
+++ script/script.js
@@ -2,10 +2,24 @@
 const currentYear = new Date().getUTCFullYear();
 $("#date").html(currentYear);
 
-// return to the top arrow
-// arrow.addEventListener("click", () => {
-//   window.scroll({
-//     top: 0,
-//     behavior: "smooth",
-//   });
-// });
+// handling change on navigation items when scroll
+const sections = document.querySelectorAll("section");
+const navItems = document.querySelectorAll(".nav-item");
+document.addEventListener("DOMContentLoaded", function () {
+  const sections = document.querySelectorAll("section");
+  const navLinks = document.querySelectorAll("nav .nav-item a");
+
+  function changeActiveLink() {
+    let index = sections.length;
+
+    while (--index && window.scrollY + 50 < sections[index].offsetTop) {}
+
+    navLinks.forEach((link) => link.classList.remove("active"));
+    if (navLinks[index]) {
+      navLinks[index].classList.add("active");
+    }
+  }
+
+  changeActiveLink(); // Run on initial load
+  window.addEventListener("scroll", changeActiveLink);
+});
